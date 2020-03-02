c6aa042183a4834c4da037aa1f439b682461adb9
Glocker134 <glocker134@gmail.com>
2020-03-01 22:37:52 -0400

    Added assignment 1 files.

---
+++ lesson_1/cash.c
@@ -0,0 +1,57 @@
+#include <math.h>
+#include <stdio.h>
+#include <cs50.h>
+
+void change(void);
+
+int main(void) 
+{
+    change();    
+}
+
+void change(void)
+{
+    const int penny = 1;
+    const int nickel = 5;
+    const int dime = 10;
+    const int quarter = 25;
+    int coins = 0;
+    float input;
+    do
+    {
+        input = get_float("Change owed: ");
+    }
+    while (input < 0);
+    int fixed = round(input * 100);
+    while (fixed > 0)
+    {
+        if (fixed / quarter > 0)
+        {
+            fixed -= quarter;
+            coins++;
+        }
+        else if (fixed / dime > 0)
+        {
+            fixed -= dime;
+            coins++;
+        }
+        else if (fixed / nickel > 0)
+        {
+            fixed -= nickel;
+            coins++;
+        }
+        else if (fixed / penny > 0)
+        {
+            fixed -= penny;
+            coins++;
+        }
+    }
+    printf("%i\n", coins);
+}
+
+/*
+const int penny = 1;
+const int nickel = 5;
+const int dime = 10;
+const int quarter = 25;
+*/
---
+++ lesson_1/mario_more.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+void blank(void);
+void reverse(int h);
+
+int main(void) 
+{
+    int input;
+    do 
+    {
+        input = get_int("Height: ");
+    }
+    while (input < 1 || input > 8);
+    draw(input);
+}
+
+void draw(int n) 
+{
+    //if (n == 1) printf("#\n");
+    //else {
+    int count = n - 1;
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            if (j < count)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        blank();
+        reverse(n - count);
+        printf("\n");
+        count--;
+    }
+}
+
+
+void blank(void) 
+{
+    printf("  ");
+}
+
+void reverse(int h) 
+{
+    for (int i = 0; i < h; i++) 
+    {
+        printf("#");
+    }
+}

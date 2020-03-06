3e6db71c61995732ab449dbcb64e2abbf6b27e6a
Glocker134 <glocker134@gmail.com>
2020-03-05 23:39:11 -0400

    Added lesson 2 folder and readability file.

---
+++ lesson_2/readability.c
@@ -0,0 +1,55 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    int wordCount = 0;
+    int sentenceCount = 0;
+    int letterCount = 0;
+    int index;
+    float letterAvg, sentenceAvg;
+    char dotChar = '.';
+    char questChar = '?';
+    char exclChar = '!';
+
+    string text = get_string("Text: ");
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letterCount++;
+        }
+
+        if (text[i] == dotChar || text[i] == questChar || text[i] == exclChar)
+        {
+            sentenceCount++;
+        }
+
+        if (isspace(text[i]) || iscntrl(text[i + 1]))
+        {
+            wordCount++;
+        }
+    }
+
+    letterAvg = (letterCount * 100) / wordCount;
+    sentenceAvg = (sentenceCount * 100) / wordCount;
+
+    index = round(0.0588 * letterAvg - 0.296 * sentenceAvg - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}

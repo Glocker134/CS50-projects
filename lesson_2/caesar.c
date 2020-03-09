6689bb826f9739968605979a2cf52893e84d56ea
Glocker134 <glocker134@gmail.com>
2020-03-08 23:08:48 -0400

    Finished caesar assignment.

--- lesson_2/caesar.c
+++ lesson_2/caesar.c
@@ -4,26 +4,107 @@
 #include <string.h>
 #include <ctype.h>
 
+int validate(string);
 int cipher(int);
 
 int main(int argc, string argv[])
 {
-    if (argv[1] && (atoi(argv[1]) > 0))
+    if (argc == 2 && validate(argv[1]))
     {
-        // Validate if it's a number
-        // Validate if the number doesn't have whitespaces (PENDING)
-        for (int i = 0; i < strlen(argv[1]); i++)
         int key = atoi(argv[1]);
         cipher(key);
+        return 0;
     }
     else
     {
         printf("Usage: ./caesar key.\n");
+        return 1;
     }
 }
 
 int cipher(int key)
 {
-    printf("this is cipher. Do stuff here\n");
+    string input = get_string("plaintext: ");
+
+    if (key % 26 == 0)
+    {
+        printf("ciphertext: %s\n", input);
+    }
+    else
+    {
+        char output[strlen(input)];
+        char aux;
+
+        if (key > 26)
+        {
+            // Changing key to a value between 1 and 25:
+            key = key % 26;
+        }
+        for (int i = 0; i < strlen(input); i++)
+        {
+            int caps = 0;
+
+            // Checking if the character is a whitespace or punctuation character
+            if (isalpha(input[i]))
+            {
+                aux = tolower(input[i]);
+                // Checking if the original character is uppercase
+                if (aux != input[i])
+                {
+                    caps = 1;
+                }
+
+                // Checking if the cipher goes beyond z
+                if (aux + key > 122)
+                {
+                    int posAux = (aux + key - 26);
+                    aux = posAux;
+                }
+                else
+                {
+                    aux += key;
+                }
+
+                // Reverting the caps check
+                if (caps == 1)
+                {
+                    aux = toupper(aux);
+                    output[i] = aux;
+                }
+                else
+                {
+                    output[i] = aux;
+                }
+            }
+            else
+            {
+                output[i] = input[i];
+            }
+        }
+        
+        // Adding a null at the end of output
+        output[strlen(input)] = '\0';
+        printf("ciphertext: %s\n", output);
+    }
     return 0;
+}
+
+int validate(string key)
+{
+    if (atoi(key) > 0)
+    {
+        for (int i = 0; i < strlen(key); i++)
+        {
+            if (!(isdigit(key[i])))
+            {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }

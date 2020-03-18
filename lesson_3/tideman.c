c5834af1a87b8e606007ba4a38a6e71550b1b0eb
Glocker134 <glocker134@gmail.com>
2020-03-17 23:15:03 -0400

    Finished the merge sort function.

--- lesson_3/tideman.c
+++ lesson_3/tideman.c
@@ -8,7 +8,7 @@
 // preferences[i][j] is number of voters who prefer i over j
 int preferences[MAX][MAX];
 
-// Setting a flag to initialize the preferences array on first check later
+// Setting a flag to initialize the preferences array on first run
 bool preferences_initialized = false;
 
 // locked[i][j] means i is locked in over j
@@ -39,6 +39,8 @@ void print_winner(void);
 
 // My added prototypes
 void initialize_preferences(void);
+void merge_sort(pair array[], int left, int middle, int right);
+void merge_setup(pair array[], int left, int right);
 
 
 int main(int argc, string argv[])
@@ -157,6 +159,7 @@ void add_pairs(void)
                 pairs[count].loser = i;
             }
             count++;
+            pair_count++
         }
     }
     return;
@@ -165,9 +168,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TO DO:
-    // Reverse merge sort
-    // Check notebook!!
+    merge_setup(pairs, 0, pair_count - 1);
     return;
 }
 
@@ -196,3 +197,70 @@ void initialize_preferences(void)
     }
 }
 
+void merge_sort(pair array[], int left, int middle, int right)
+{
+    int left_index, right_index, array_index;
+    int left_size = middle - left + 1;
+    int right_size = right - middle;
+    pair leftA[left_size];
+    pair rightA[right_size];
+
+    for (int i = 0; i < left_size; i++)
+    {
+        leftA[i] = array[left + i];
+    }
+
+    for (int j = 0; j < right_size; j++)
+    {
+        rightA[j] = array[middle + 1 + j];
+    }
+
+    left_index = 0;
+    right_index = 0;
+    array_index = left;
+
+    while (left_index < left_size && right_index < right_size)
+    {
+        int left_value = preferences[leftA[left_index].winner][leftA[left_index].loser] - preferences[leftA[left_index].loser][leftA[left_index].winner];
+        int right_value = preferences[rightA[right_index].winner][rightA[right_index].loser] - preferences[rightA[right_index].loser][rightA[right_index].winner];
+
+        if (left_value >= right_value)
+        {
+            array[array_index] = leftA[left_index];
+            left_index++;
+        }
+        else
+        {
+            array[array_index] = rightA[right_index];
+            right_index++;
+        }
+        array_index++;
+    }
+
+    while (left_index < left_size)
+    {
+        array[array_index] = leftA[left_index];
+        left_index++;
+        array_index++;
+    }
+
+    while (right_index < right_size)
+    {
+        array[array_index] = rightA[right_index];
+        right_index++;
+        array_index++;
+    }
+
+}
+
+void merge_setup(pair array[], int left, int right)
+{
+    if (left < right)
+    {
+        int middle = (left + right) / 2;
+        merge_setup(array, left, middle);
+        merge_setup(array, middle + 1, right);
+
+        merge_sort(array, left, middle, right);
+    }
+}

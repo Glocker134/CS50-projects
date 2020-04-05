486682859f9ed89baaf8541e5466e4a74797120b
Glocker134 <glocker134@gmail.com>
2020-04-04 22:08:39 -0400

    Added hello, mario-less, and mario-more

---
+++ lesson_6/hello.py
@@ -0,0 +1,6 @@
+# Get the user's name and greet them
+
+from cs50 import get_string
+
+s = get_string("What is your name?\n")
+print(f"Hello, {s}")
---
+++ lesson_6/mario-more.py
@@ -0,0 +1,27 @@
+# Print out a double half-pyramid of a specified height
+
+from cs50 import get_int
+
+# Adapting my old C code to Python
+
+# Receiving input from user, will repeat until the received value is a value between 1 and 8
+while True:
+    size = get_int("Height: ")
+    # Range can receive an optional starting value to the left of the end value
+    if size in range(1, 9):
+        break
+
+# Printing each floor and adding whitespace when required
+count = size - 1
+for i in range(size):
+    for j in range(size):
+        if j < count:
+            print(" ", end="")
+        else:
+            print("#", end="")
+    # Whitespace between pyramids
+    print("  ", end="")
+    # Completing the other side of the pyramid
+    print("#" * (i + 1), end="")
+    print()
+    count -= 1
